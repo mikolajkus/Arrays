bb1d0d1f0ef47a74f571002228e3b92845714be9
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-19 19:49:51 +0200

    Added the first 4 exercises

--- script.js
+++ script.js
@@ -15,9 +15,55 @@ console.log(
 // 2. Write the getSmallestNumber function.
 // write two versions of the above function: one using a for  loop and one using the sort  function.
 
-function getSmallestNumber(arrayOfNumbers) {
-    
+function getSmallestNumberBySort(arrayOfNumbers) {
+    const sortedArray = arrayOfNumbers.sort(
+        function(a, b) {
+            return a - b;
+        }
+    );
+    return sortedArray[0];
 }
 
-console.log(getSmallestNumber([2, -5, 10, 1, 4])); // -5
-console.log(getSmallestNumber([200, 25, 4, 123, 87])); // 4
+console.log(getSmallestNumberBySort([2, -5, 10, 1, 4])); // -5
+console.log(getSmallestNumberBySort([200, 25, 4, 123, 87])); // 4
+
+function getSmallestNumberByForLoop(arrayOfNumbers) {
+    let smallestNumber = arrayOfNumbers[0];
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        if (arrayOfNumbers[i] < smallestNumber) {
+            smallestNumber = arrayOfNumbers[i];
+        }
+    }
+    return smallestNumber;
+}
+
+console.log(getSmallestNumberByForLoop([2, -5, 10, 1, 4])); // -5
+console.log(getSmallestNumberByForLoop([200, 25, 4, 123, 87])); // 4
+
+// 3. Write the getSquaredNumbers function.
+// don't modify the array passed as an argument.
+
+function getSquaredNumbers(arrayOfNumbers) {
+    return arrayOfNumbers.map(
+        function (number) {
+            return number ** 2;
+        }
+    )
+}
+
+console.log(getSquaredNumbers([1, 2, 3, 4, 5])); // [1, 4, 9, 16, 25]
+console.log(getSquaredNumbers([6, 7, 8, 9, 10])); // [36, 49, 64, 81, 100]
+
+const numbers = [1, 2, 3];
+const squaredNumbers = getSquaredNumbers(numbers);
+console.log(squaredNumbers); // [1, 4, 9]
+console.log(numbers); // [1, 2, 3]
+
+// 4. Write the getReversedString  function.
+
+function getReversedString(string) {
+    return string.split('').reverse().join('');
+}
+
+console.log(getReversedString('Hello!')); // '!olleH'
+console.log(getReversedString('Arrays')); // 'syarrA'
