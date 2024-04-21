5a0f5daff22cb7e78635436b4ca429adbe69acb9
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-21 21:16:21 +0200

    Added the exercise number 20 from the codewars

--- script.js
+++ script.js
@@ -344,3 +344,17 @@ function getAverage(marks){
 }
 
 console.log(getAverage([2, 3, 4, 5]));
+
+// 20. You take your son to the forest to see the monkeys.
+// You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+function getMonkeyCount(number) {
+    let countedMonkey = [];
+    for (let i = 1; i <= number; i++) {
+        countedMonkey.push(i);
+    }
+    return countedMonkey;
+}
+
+console.log(getMonkeyCount([10]));
