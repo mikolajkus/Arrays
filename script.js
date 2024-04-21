ef4c0697b90c2f743375ac5990f5ce47ff48196a
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-21 16:09:20 +0200

    Added the another one exercise number 10, the exercise is from codewars

--- script.js
+++ script.js
@@ -185,3 +185,22 @@ function getSquareSum(arrayOfNumbers) {
 }
 
 console.log(getSquareSum([1, 2, 2]));
+
+// 10. Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+function getInitials(name) {
+    const names = name.split(' ');
+    let initials = names[0].substring(0, 1).toUpperCase();
+    if (names.length > 1) {
+        initials += '.' + names[names.length - 1].substring(0, 1).toUpperCase();
+    }
+    return initials;
+}
+
+// Example usage
+console.log(getInitials("Sam Harris")); // Output: "S.H"
+console.log(getInitials("patrick feeney")); // Output: "P.F"
