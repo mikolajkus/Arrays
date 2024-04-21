c89d46d461b5b00aea9cd4441175261bb657d0ec
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-21 16:42:53 +0200

    Added the another one exercise, the exercise number 11, exercise is from codewars

--- script.js
+++ script.js
@@ -204,3 +204,24 @@ function getInitials(name) {
 // Example usage
 console.log(getInitials("Sam Harris")); // Output: "S.H"
 console.log(getInitials("patrick feeney")); // Output: "P.F"
+
+// 11. Consider an array/list of sheep where some sheep may be missing from their place.
+// We need a function that counts the number of sheep present in the array (true means present).
+
+function getCountSheeps(sheep) {
+    let numberOfSheeps = 0;
+    for (let i = 0; i < sheep.length; i++) {
+        if (sheep[i] === true) {
+            numberOfSheeps++;
+        }
+    }
+    return numberOfSheeps;
+}
+
+console.log(getCountSheeps(
+    [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]));
