2112b6e30fbdd2a5cec682e7996b9467e4f2aba2
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-21 20:23:30 +0200

    Added the exercise number 17 from codewars

--- script.js
+++ script.js
@@ -294,3 +294,19 @@ function getSumOfNumbers(numbers) {
 }
 
 console.log(getSumOfNumbers([]));
+
+// 17. I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function getTheSumOfTwoArrays(firstArray, secondArray) {
+    let sumOfTwoArrays = 0;
+    for (let i = 0; i < firstArray.length; i++) {
+        sumOfTwoArrays += firstArray[i];
+    }
+    for (let i = 0; i < secondArray.length; i++) {
+        sumOfTwoArrays += secondArray[i];
+    }
+    return sumOfTwoArrays;
+}
+
+console.log(getTheSumOfTwoArrays([1, 2, 3], [4, 5, 6]));
